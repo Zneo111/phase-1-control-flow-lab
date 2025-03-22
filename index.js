e0bb4579abcfc802e8a486c401e55e623bcebdf5
Zneo111 <tevin.rutere@student.moringaschool.com>
2025-03-22 18:33:12 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-function scuberGreetingForFeet(){
-  it('gives customers a free sample if the ride is less than or equal to 400 feet', function () {
-    expect(scuberGreetingForFeet(199)).to.equal('This one is on me!');
-  });
-  function scuberGreetingForFeet(distance) {
+function scuberGreetingForFeet(distance){
     if (distance <= 400) {
       return 'This one is on me!';
     } else if (distance > 400 && distance <= 2000) {
@@ -14,26 +10,23 @@ function scuberGreetingForFeet(){
     }
   }
 
-}
 
-
-function ternaryCheckCity(){
-  function ternaryCheckCity(city) {
+function ternaryCheckCity(city){
     return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
   }
 
-}
 
-function switchOnCharmFromTip(){
-  function switchOnCharmFromTip(tip) {
+
+function switchOnCharmFromTip(tip) {
+    tip = Number(tip);
+
     switch (true) {
-      case (tip === 0):
-        return 'No tip.';
-      case (tip > 0 && tip <= 500):
-        return 'Thank you so much.';
       case (tip > 500):
-        return 'You are too generous!';
+        return 'Thank you so much.';
+      case (tip > 0 && tip <= 500):
+        return 'Thank you.';
+      default:
+        return 'Bye.';
     }
   }
 
-}
