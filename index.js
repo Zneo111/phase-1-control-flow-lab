db3bd46156f3c8ac375fee2f5f5a836d81121beb
Zneo111 <tevin.rutere@student.moringaschool.com>
2025-04-03 10:59:08 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
-function scuberGreetingForFeet(distance){
-    if (distance <= 400) {
-      return 'This one is on me!';
-    } else if (distance > 400 && distance <= 2000) {
-      return 'That will be twenty bucks.';
-    } else if (distance > 2000 && distance <= 2500) {
-      return 'I will gladly take your thirty bucks.';
-    } else {
-      return 'No can do.';
-    }
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return "This one is on me!";
+  } else if (distance > 400 && distance <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (distance > 2000 && distance <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
   }
+}
 
+function ternaryCheckCity(city) {
 
-function ternaryCheckCity(city){
-    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
-  }
-
-
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+}
 
 function switchOnCharmFromTip(tip) {
-    tip = Number(tip);
-
-    switch (true) {
-      case (tip > 500):
-        return 'Thank you so much.';
-      case (tip > 0 && tip <= 500):
-        return 'Thank you.';
-      default:
-        return 'Bye.';
-    }
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
   }
-
+}
